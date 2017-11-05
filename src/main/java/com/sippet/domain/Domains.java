7713b4d70a048580df91e8ba3e8dd36179ef92bf
Inho Choi <inhochoi@Inhoui-MacBook-Pro.local>
2017-11-05 13:32:59 +0900

    first commit

---
+++ src/main/java/com/sippet/domain/Domains.java
@@ -0,0 +1,4 @@
+package com.sippet.domain;
+
+public interface Domains {
+}
---
+++ src/main/java/com/sippet/web/HelloController.java
@@ -0,0 +1,15 @@
+package com.sippet.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/")
+    public String hello(@RequestParam(value="name", required=false, defaultValue="Sippet") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
