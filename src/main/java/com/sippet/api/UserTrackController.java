2a2f48cc3c73f120cb3939840b34da52dddb7f56
donnaknew <donnaknew@gmail.com>
2017-12-23 15:52:43 +0900

    UserTrack 컨트롤러 구현 및 쿠키 생성, NullResolver 구현 (#6)
    * Tracking User
    
    * Bake Cookie
    
    * ArrayList Null Check

--- src/main/java/com/sippet/api/UserTrackController.java
+++ src/main/java/com/sippet/api/UserTrackController.java
@@ -1,12 +1,17 @@
 package com.sippet.api;
 
 import com.sippet.domain.usertrack.UserTrack;
-import com.sippet.domain.usertrack.UserTrackRepository;
 import com.sippet.domain.usertrack.UserTrackPathNameCount;
+import com.sippet.domain.usertrack.UserTrackRepository;
+import com.sippet.service.TrackingResolver;
+import com.sippet.service.TrackingResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
+
 
 @RestController
 @RequestMapping("/api/track")
@@ -14,9 +19,38 @@ public class UserTrackController {
     @Autowired
     private UserTrackRepository userTrackRepository;
 
+    @Autowired
+    private TrackingResolver trackingResolver;
+
     @PostMapping(path = "/")
-    public UserTrack addTrack(@RequestBody UserTrack userTrack) {
-        return userTrackRepository.save(userTrack);
+    public void addTrack(@RequestBody UserTrackDto userTrackDto,
+                         @CookieValue(value = "newVisitor", required = false) Boolean newVisistor,
+                         @CookieValue(value = "trackingId", required = false) String trackingId,
+                         HttpServletResponse httpServletResponse) {
+        final TrackingResult trackingResult = trackingResolver.resolve(newVisistor, trackingId);
+
+        bakeTrackingCookie(httpServletResponse, trackingResult);
+
+        final UserTrack userTrack = UserTrack.builder()
+                .pathName(userTrackDto.getPathName())
+                .referrer(userTrackDto.getReferrer())
+                .href(userTrackDto.getHref())
+                .host(userTrackDto.getHost())
+                .trackingId(trackingResult.getTrackingId())
+                .newVisitor(trackingResult.getNewVisitor())
+                .build();
+
+        userTrackRepository.save(userTrack);
+    }
+
+    private void bakeTrackingCookie(HttpServletResponse httpServletResponse, TrackingResult trackingResult) {
+        final Cookie newVisitorCookie = new Cookie("newVisitor", trackingResult.getNewVisitor().toString());
+        newVisitorCookie.setMaxAge(10 * 365 * 24 * 60 * 60);
+        final Cookie trackingIdCookie = new Cookie("trackingId", trackingResult.getTrackingId());
+        trackingIdCookie.setMaxAge(10 * 365 * 24 * 60 * 60);
+
+        httpServletResponse.addCookie(newVisitorCookie);
+        httpServletResponse.addCookie(trackingIdCookie);
     }
 
     @GetMapping(path = "/group_by_count/path_name")
