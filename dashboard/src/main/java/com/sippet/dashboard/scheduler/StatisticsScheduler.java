8eb8a1cfeb3ee7c51822b36ed8928beb84fc5177
Kim Young Ho <donnaknew@gmail.com>
2018-05-07 11:10:42 +0900

    Add temp file

--- dashboard/src/main/java/com/sippet/dashboard/scheduler/StatisticsScheduler.java
+++ dashboard/src/main/java/com/sippet/dashboard/scheduler/StatisticsScheduler.java
@@ -1,13 +1,21 @@
 package com.sippet.dashboard.scheduler;
 
+import com.sippet.domain.domain.userstatistics.UserStatistics;
+import com.sippet.domain.domain.userstatistics.UserStatisticsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class StatisticsScheduler {
+    @Autowired
+    UserStatisticsRepository userStatisticsRepository;
+
     @Scheduled(cron = "*/10 * * * * *")
     public void statisticsScheduling() {
         System.out.println("statisticsScheduling method!!!");
+
+
     }
 }
--- domain/src/main/java/com/sippet/domain/domain/userstatistics/UserStatisticsRepository.java
+++ domain/src/main/java/com/sippet/domain/domain/userstatistics/UserStatisticsRepository.java
@@ -1,8 +1,19 @@
 package com.sippet.domain.domain.userstatistics;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserStatisticsRepository extends JpaRepository<UserStatistics, Long> {
+    @Query(value = "SELECT COUNT(DISTINCT u.trackingId) FROM UserTrack u")
+    int countTotalVisitor();
+
+    @Query(value = "SELECT COUNT(DISTINCT u.trackingId) FROM UserTrack u")
+    List<Integer> countPathVisitor();
+
+    @Query(value = "SELECT COUNT(DISTINCT u.trackingId) as count FROM UserTrack u WHERE u.newVisitor = true")
+    int findCountNewVisitor();
 }
